f4ab0e6f0b11893fec46a9e6a0114eec07b7b5b3
j1li <iotapsai@gmail.com>
2024-10-13 02:13:59 -0400

    🚧 new features functionality update

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -7,40 +7,115 @@ const events = [
     { "Dates": "Wed Dec 18 2024", "Venue": "Press Club", "Location": "San Francisco, CA" }
 ];
 
-const concertCardcontainer = document.querySelector(".concert__card");
-const concertTablecontainer = document.querySelector(".concert__table");
 
-function loadConcertDetails() {
-    concertCardcontainer.innerHTML = "";
-    concertTablecontainer.innerHTML = "";
+const concertCardContainer = document.querySelector(".concert__card");
+const concertTableContainer = document.querySelector(".concert__table");
 
-    events.forEach(e => {
-        const card = document.createElement("div");
-        card.className = "concert__card--item";
-        card.innerHTML = `
-            <p>${e.Dates}</p>
-            <p>${e.Venue}</p>
-            <p>${e.Location}</p>
-            <button class="buy-ticket">Buy Ticket</button>
-        `;
-        concertCardcontainer.appendChild(card);
-    });
+function createConcertCard(concert) {
+    const Concertcard = document.createElement("div");
+    Concertcard.className = "concert__card--item";
 
-    const table = document.createElement("table");
-    const headerRow = `<tr><th>Date</th><th>Venue</th><th>Location</th></tr>`;
-    table.innerHTML = headerRow;
+    const dateDt = document.createElement("p");
+    dateDt.innerText = concert.Dates;
 
-    events.forEach(e => {
-        const row = `<tr>
-            <td>${e.Dates}</td>
-            <td>${e.Venue}</td>
-            <td>${e.Location}</td>
-         <td><button class="buy-ticket">Buy Ticket</button></td>
-        </tr>`;
-        table.innerHTML += row;
-    });
+    const venueTxt = document.createElement("p");
+    venueTxt.innerText = concert.Venue;
 
-    concertTablecontainer.appendChild(table);
+    const locationTxt = document.createElement("p");
+    locationTxt.innerText = concert.Location;
+
+    const buyTicketBtn = document.createElement("button");
+    buyTicketBtn.className = "btn_ticekt";
+    buyTicketBtn.innerText = "Buy Ticket";
+
+    buyTicketBtn.addEventListener("click", () => BuyTickethandler(concert));
+
+    Concertcard.appendChild(dateDt);
+    Concertcard.appendChild(venueTxt);
+    Concertcard.appendChild(locationTxt);
+    Concertcard.appendChild(buyTicketBtn);
+
+    return Concertcard;
 }
 
-loadConcertDetails();
+function createConcertTable(concerts) {
+    const ConcertTable = document.createElement("table");
+
+    const headerRow = document.createElement("tr");
+    const headers = ["Date", "Venue", "Location", "Button"];
+    headers.forEach(headerText => {
+        const th = document.createElement("th");
+        th.innerText = headerText;
+        headerRow.appendChild(th);
+    });
+    ConcertTable.appendChild(headerRow);
+
+    concerts.forEach(concert => {
+        const row = document.createElement("tr");
+
+        const dateCell = document.createElement("td");
+        dateCell.innerText = concert.Dates;
+
+        const venueCell = document.createElement("td");
+        venueCell.innerText = concert.Venue;
+
+        const locationCell = document.createElement("td");
+        locationCell.innerText = concert.Location;
+
+        const btnCell = document.createElement("td");
+        const buyTicketBtn = document.createElement("button");
+        buyTicketBtn.className = "btn_ticket";
+        buyTicketBtn.innerText = "Buy Ticket";
+        btnCell.appendChild(buyTicketBtn);
+
+        buyTicketBtn.addEventListener("click", () => BuyTickethandler(concert));
+
+        row.appendChild(dateCell);
+        row.appendChild(venueCell);
+        row.appendChild(locationCell);
+        row.appendChild(btnCell);
+        ConcertTable.appendChild(row);
+    });
+
+    return ConcertTable;
+}
+
+function renderConcertDetails() {
+    concertCardContainer.innerHTML = "";
+    concertTableContainer.innerHTML = "";
+
+    events.forEach(event => {
+        const card = createConcertCard(event);
+        concertCardContainer.appendChild(card);
+    });
+
+    const table = createConcertTable(events);
+    concertTableContainer.appendChild(table);
+}
+
+
+function BuyTickethandler(concert) {
+    /*
+    Source：
+    https://stackoverflow.com/a/47177899
+    https://community.retool.com/t/fetch-image-and-pass-base64-to-api/31662
+    */
+    const url = '../assets/partials/status_code_bird.txt';
+
+    fetch(url)
+        .then(response => {
+            return response.text();
+        })
+        .then(base64String => {
+            console.log('%c ', `padding: 100px; background: url(${base64String}); background-size: contain;`,"\r\nFetch concert data:\n", concert);
+        })
+        .catch(error => {
+            console.error('Error loading the Base64 image:', error);
+        });
+}
+
+
+renderConcertDetails();
+
+
+
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,18 +1,106 @@
-//Load gallery
-function createImageElements(count) {
+const comments = [
+    {
+        "author": "Victor Pinto",
+        "date": "11/02/2023",
+        "content": "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        "author": "Christina Cabrera",
+        "date": "10/28/2023",
+        "content": "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+    },
+    {
+        "author": "Isaac Tadesse",
+        "date": "10/20/2023",
+        "content": "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    }];
+
+
+function createImageElement(idx) {
+    const img = document.createElement('img');
+    img.className = `photo-gallery-${idx}`;
+    img.alt = `Photo Gallery ${idx}`;
+    return img;
+}
+
+function createImageElements(ctr) {
     const gallery = document.getElementById('photo-gallery');
     gallery.innerHTML = '';
 
-    for (let i = 1; i <= count; i++) {
-        const img = document.createElement('img');
-        img.className = `photo-gallery-${i}`;
-        img.alt = `Photo Gallery ${i}`;
+    for (let i = 1; i <= ctr; i++) {
+        const img = createImageElement(i);
         gallery.appendChild(img);
     }
 }
 
-//execute function after complete page load
-document.addEventListener('DOMContentLoaded', function() {
-    createImageElements(9);
+const commentForm = document.getElementById('comment-form');
+const commentsFeed = document.getElementById('comments-feed');
 
-});
+function createCommentCard(comment) {
+    const commentElement = document.createElement('article');
+    commentElement.classList.add('comment');
+
+    const avatarSection = document.createElement('section');
+    avatarSection.classList.add('comments__feed-avatar');
+    const avatarPlaceholder = document.createElement('div');
+    avatarPlaceholder.classList.add('avatar-placeholder');
+    avatarSection.appendChild(avatarPlaceholder);
+
+    const contentSection = document.createElement('section');
+    contentSection.classList.add('comments__feed-content');
+
+    const authorDateContainer = document.createElement('div');
+    authorDateContainer.classList.add('comments__feed-authorDate');
+
+    const authorTxt = document.createElement('p');
+    authorTxt.innerText = comment.author;
+
+    const dateDT = document.createElement('p');
+    dateDT.innerText = comment.date;
+
+    authorDateContainer.appendChild(authorTxt);
+    authorDateContainer.appendChild(dateDT);
+
+    const contentTxt = document.createElement('p');
+    contentTxt.innerText = comment.content;
+
+    contentSection.appendChild(authorDateContainer);
+    contentSection.appendChild(contentTxt);
+
+    commentElement.appendChild(avatarSection);
+    commentElement.appendChild(contentSection);
+
+
+    return commentElement;
+}
+
+function renderCommentCard() {
+    commentsFeed.innerHTML = '';
+    comments.forEach(comment => {
+        const commentCard = createCommentCard(comment);
+        commentsFeed.appendChild(commentCard);
+    });
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const nameInput = event.target.fullName.value;
+    const commentInput = event.target.comment.value;
+
+    const newComment = {
+        author: nameInput,
+        date: new Date().toLocaleDateString(),
+        content: commentInput
+    };
+
+    comments.push(newComment);
+    renderCommentCard();
+
+    nameInput.value = '';
+    commentInput.value = '';
+}
+
+commentForm.addEventListener('submit', handleFormSubmit);
+renderCommentCard();
+createImageElements(9);
